886298c75a1caf15281aad9fed34a2c29f42a724
tedlin0913 <ptengineerlin@gmail.com>
2024-06-15 23:50:24 +0800

    update navbar behavior

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,26 +1,27 @@
 var navbar = document.querySelector('#mainNavbar')
 
 
+// Index (home) page
+window.onscroll = function () {
 
+    // Only want a fixed top in the main page, where the navbar become tranparent
+    // Other time want a sticky-top so don't have to deal with margins
+    if (window.scrollY > 0) {
+        navbar.classList.add('scrolled')
+    } else {
+        navbar.classList.remove('scrolled')
+    }
+}
 
 // Only use fixed top navbar on Home page
 if (window.location.pathname == '/') {
-    // Index (home) page
-    window.onscroll = function () {
-
-        // Only want a fixed top in the main page, where the navbar become tranparent
-        // Other time want a sticky-top so don't have to deal with margins
-        if (window.scrollY > 0) {
-            navbar.classList.add('scrolled')
-        } else {
-            navbar.classList.remove('scrolled')
-        }
-    }
+    window.onscroll()
 
 } else {
     // Other page
     navbar.classList.remove('fixed-top')
     navbar.classList.add('sticky-top')
+    window.onscroll()
 
     console.log(window.location.pathname);
 }
